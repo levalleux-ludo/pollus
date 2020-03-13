db0b9e9886ba792950a8d79ab2d52faa1ad5d693
Ludovic Levalleux <levalleux_ludo@hotmail.com>
2020-03-13 18:49:14 +0000

    Fix unit tests that expect an exception

--- token/test/votus.spec.js
+++ token/test/votus.spec.js
@@ -1,9 +1,21 @@
 const chai = require('chai')
 const chaiAsPromised = require('chai-as-promised')
-chai.use(chaiAsPromised)
+chai.use(chaiAsPromised).should();
 const { expect, assert } = chai
 
 var Votus = artifacts.require("Votus");
+
+
+const ERROR_GENERIC = 'VM Exception while processing transaction\: revert';
+const REASON_GIVEN = '\-\- Reason given\:';
+
+function createExceptionMessage(reason, addReasonGiven = true) {
+    if (addReasonGiven) {
+        return new RegExp(ERROR_GENERIC + ' ' + reason + ' ' + REASON_GIVEN + ' ' + reason);
+    } else {
+        return new RegExp(ERROR_GENERIC + ' ' + reason);
+    }
+}
 
 contract('Testing Votus contract', function(accounts) {
 
@@ -35,36 +47,56 @@ contract('Testing Votus contract', function(accounts) {
 
     const account4 = accounts[4]
 
+    var donator = undefined
+
+    // const ERROR_CREATOR_ONLY = ERROR_GENERIC + ' ' + 'this method can only be called by the poll creator \-\- Reason given\: this method can only be called by the poll creator';
+    const ERROR_CREATOR_ONLY = createExceptionMessage('this method can only be called by the poll creator');
+    // const ERROR_POLL_ALREADY_EXIST = 'VM Exception while processing transaction\: revert poll already exist \-\- Reason given\: poll already exist';
+    const ERROR_POLL_ALREADY_EXIST = createExceptionMessage('poll already exist');
+    const ERROR_POLL_NOT_EXIST = createExceptionMessage('poll does not exist');
+    const ERROR_VOTE_NOT_ENDED = createExceptionMessage('the vote shall be ended to get the results', false);
+
 
     it('should be able to create a poll', async() => {
         token = await Votus.new()
         await token.createPoll(pollId1, { from: accounts[0] });
         expect(await token.pollIsEnded(pollId1)).to.be.false
     })
-    it('should be able to get the contract ETH balance', async() => {
+    it('should be able to find an humble donator to give 1 ETH to my contract under test', async() => {
         let balance = await web3.eth.getBalance(token.address)
         expect(balance).to.equal("0")
-        await token.sendTransaction({ from: accounts[0], value: 10000000000000000000 });
-        balance = await web3.eth.getBalance(token.address)
-        expect(balance).to.equal("10000000000000000000")
+        donator = undefined;
+        let minimumBalance = web3.utils.toWei('2', 'ether')
+        for (let i = 0; accounts[i] && !donator; i++) {
+            await web3.eth.getBalance(accounts[i]).then((accountBalance) => {
+                if (+accountBalance > minimumBalance) {
+                    donator = accounts[i];
+                }
+            })
+        }
+        expect(donator, 'no more account owning enough ETH. Please create another Ganache workspace').to.not.be.undefined
+    })
+    it('should be able to get the contract ETH balance', async() => {
+        const _1ETH = web3.utils.toWei('1', 'ether')
+        await token.sendTransaction({ from: donator, value: _1ETH });
+        let balance = await web3.eth.getBalance(token.address)
+        expect(balance).to.equal(_1ETH)
     })
 
-    // it('should not be able to create a poll already existing', async () => {
-    //     const ret = await token.createPoll(pollId1, {from: accounts[0]})
-    //     // expect(await token.createPoll(pollId1)).to.be.rejectedWith(/VM Exception while processing transaction: revert poll already exist -- Reason given: poll already exist/)
-    //     expect(ret).to.be.rejectedWith(Error)
-    // })
+    it('should not be able to create a poll already existing', async() => {
+        await token.createPoll(pollId1, { from: accounts[0] }).should.be.rejectedWith(ERROR_POLL_ALREADY_EXIST);
+    })
     it('should be able to end a poll if creator', async() => {
-            await token.createPoll(pollId99, { from: accounts[0] });
-            expect(await token.pollIsEnded(pollId99)).to.be.false
-            token.endPoll(pollId99, { from: accounts[0] })
-            expect(await token.pollIsEnded(pollId99)).to.be.true
-        })
-        // it('should not be able to end a poll if not creator', async () => {
-        //     await token.createPoll(pollId2, {from: accounts[0]});
-        //     expect(await token.pollIsEnded(pollId2)).to.be.false
-        //     expect(await token.endPoll(pollId2, {from: accounts[1]})).to.be.rejected
-        // })
+        await token.createPoll(pollId99, { from: accounts[0] });
+        expect(await token.pollIsEnded(pollId99)).to.be.false
+        await token.endPoll(pollId99, { from: accounts[0] })
+        expect(await token.pollIsEnded(pollId99)).to.be.true
+    })
+    it('should not be able to end a poll if not creator', async() => {
+        await token.createPoll(pollId2, { from: accounts[0] });
+        expect(await token.pollIsEnded(pollId2)).to.be.false
+        await token.endPoll(pollId2, { from: accounts[1] }).should.be.rejectedWith(ERROR_CREATOR_ONLY);
+    })
     it(' should be able to deploy and mint Votus token', async() => {
         await token.mintUniqueTokenTo(pollId1, account1, tokenId1, tokenUri1, { from: accounts[0] })
 
@@ -72,8 +104,10 @@ contract('Testing Votus contract', function(accounts) {
         expect(await token.name()).to.equal(name)
     })
     it(' should be unique', async() => {
-        const duplicateTokenID = token.mintUniqueTokenTo(pollId1, account2, tokenId1, tokenUri2, { from: accounts[0] }) //tokenId
-        expect(duplicateTokenID).to.be.rejectedWith(/VM Exception while processing transaction: revert/)
+        // const duplicateTokenID = await token.mintUniqueTokenTo(pollId1, account2, tokenId1, tokenUri2, { from: accounts[0] }) //tokenId
+        // expect(duplicateTokenID).to.be.rejectedWith(/VM Exception while processing transaction: revert/)
+        await token.mintUniqueTokenTo(pollId1, account2, tokenId1, tokenUri2, { from: accounts[0] })
+            .should.be.rejectedWith(new RegExp(ERROR_GENERIC));
     })
     it(' should allow creation of multiple unique tokens and manage ownership', async() => {
         const additionalToken = await token.mintUniqueTokenTo(pollId1, account2, tokenId2, tokenUri2, { from: accounts[0] })
@@ -87,15 +121,21 @@ contract('Testing Votus contract', function(accounts) {
         expect(await token.ownerOf(tokenId2)).to.equal(account2)
     })
     it(' should allow safe transfers', async() => {
-        const unownedTokenId = token.safeTransferFrom(account2, account3, tokenId1, { from: accounts[2] }) // tokenId
-        expect(unownedTokenId).to.be.rejectedWith(/VM Exception while processing transaction: revert/)
+        // const unownedTokenId = await token.safeTransferFrom(account2, account3, tokenId1, { from: accounts[2] }) // tokenId
+        // expect(unownedTokenId).to.be.rejectedWith(/VM Exception while processing transaction: revert/)
+        await token.safeTransferFrom(account2, account3, tokenId1, { from: accounts[2] })
+            .should.be.rejectedWith(new RegExp(ERROR_GENERIC));
 
-        const wrongOwner = token.safeTransferFrom(account1, account3, tokenId2, { from: accounts[1] }) // wrong owner
-        expect(wrongOwner).to.be.rejectedWith(/VM Exception while processing transaction: revert/)
+        // const wrongOwner = await token.safeTransferFrom(account1, account3, tokenId2, { from: accounts[1] }) // wrong owner
+        // expect(wrongOwner).to.be.rejectedWith(/VM Exception while processing transaction: revert/)
+        await token.safeTransferFrom(account1, account3, tokenId2, { from: accounts[1] })
+            .should.be.rejectedWith(new RegExp(ERROR_GENERIC));
 
         // Noticed that the from gas param needs to be the token owners or it fails
-        const wrongFromGas = token.safeTransferFrom(account2, account3, tokenId2, { from: accounts[1] }) // wrong owner
-        expect(wrongFromGas).to.be.rejectedWith(/VM Exception while processing transaction: revert/)
+        // const wrongFromGas = await token.safeTransferFrom(account2, account3, tokenId2, { from: accounts[1] }) // wrong owner
+        // expect(wrongFromGas).to.be.rejectedWith(/VM Exception while processing transaction: revert/)
+        await token.safeTransferFrom(account2, account3, tokenId2, { from: accounts[1] })
+            .should.be.rejectedWith(new RegExp(ERROR_GENERIC));
 
         await token.safeTransferFrom(account2, account3, tokenId2, { from: accounts[2] })
         expect(await token.ownerOf(tokenId2)).to.equal(account3)
@@ -114,22 +154,24 @@ contract('Testing Votus contract', function(accounts) {
 
     })
     it('should be able to assign different token to different accounts for the same poll', async() => {
-            await token.createPoll(pollId3, { from: accounts[0] });
-            await token.mintUniqueTokenTo(pollId3, account3, tokenId3, tokenUri2, { from: accounts[0] })
-            await token.mintUniqueTokenTo(pollId3, account4, tokenId4, tokenUri2, { from: accounts[0] })
-            expect((await token.getNbTokensOwnedForPoll(account3, pollId3)).toNumber()).to.equal(1)
-            expect((await token.getNbTokensOwnedForPoll(account4, pollId3)).toNumber()).to.equal(1)
-            await token.safeTransferFrom(account4, account3, tokenId4, { from: account4 })
-            expect((await token.getNbTokensOwnedForPoll(account3, pollId3)).toNumber()).to.equal(2)
-            expect((await token.getNbTokensOwnedForPoll(account4, pollId3)).toNumber()).to.equal(0)
+        await token.createPoll(pollId3, { from: accounts[0] });
+        await token.mintUniqueTokenTo(pollId3, account3, tokenId3, tokenUri2, { from: accounts[0] })
+        await token.mintUniqueTokenTo(pollId3, account4, tokenId4, tokenUri2, { from: accounts[0] })
+        expect((await token.getNbTokensOwnedForPoll(account3, pollId3)).toNumber()).to.equal(1)
+        expect((await token.getNbTokensOwnedForPoll(account4, pollId3)).toNumber()).to.equal(1)
+        await token.safeTransferFrom(account4, account3, tokenId4, { from: account4 })
+        expect((await token.getNbTokensOwnedForPoll(account3, pollId3)).toNumber()).to.equal(2)
+        expect((await token.getNbTokensOwnedForPoll(account4, pollId3)).toNumber()).to.equal(0)
 
-        })
-        // it('should not be able to vote if poll does not exist', async () => {
-        //     expect(await token.vote(pollId4, voteProposition1, {from: account3})).to.be.rejected
-        // })
-        // it('should not be able to get results if vote not ended', async () => {
-        // expect(await token.getResult(pollId3, voteProposition1)).to.be.rejected
-        // })
+    })
+    it('should not be able to vote if poll does not exist', async() => {
+        await token.vote(pollId4, voteProposition1, { from: account3 })
+            .should.be.rejectedWith(ERROR_POLL_NOT_EXIST)
+    })
+    it('should not be able to get results if vote not ended', async() => {
+        await token.getResult(pollId3, voteProposition1)
+            .should.be.rejectedWith(ERROR_VOTE_NOT_ENDED)
+    })
     it('should be able to vote as many times as owned tokens', async() => {
         await token.vote(pollId3, voteProposition1, { from: account3 });
         expect((await token.getNbTokensOwnedForPoll(account3, pollId3)).toNumber()).to.equal(1)
